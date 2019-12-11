39576b10a483479623ecf55a04e37e974f1f6f50
Navaneetha14 <neetha.rddy@gmail.com>
2019-12-10 23:26:15 -0500

    Update ProductController.java

--- cicd-poc-service/src/main/java/com/cicd/cicdpocservice/ProductController.java
+++ cicd-poc-service/src/main/java/com/cicd/cicdpocservice/ProductController.java
@@ -12,14 +12,11 @@ public class ProductController
 	@GetMapping("/product")
 	public Product getProductById()
 	{
-		i=0;
-		while(i<10)
+		int i=0;
+		
+		while(i<1)
 		{
-		i=i-1;
-		}
-		while(i<10)
-		{
-		i=i-1;
+		i=i;
 		}
 		
 		Product product = new Product();
